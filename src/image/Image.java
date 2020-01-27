85d72351b64f6ed772998fff14e836b96413f4cb
Jc Tarot <jctarot.gep@gmail.com>
2020-01-27 12:13:30 +0100

    app Image version 0.2

--- src/image/Image.java
+++ src/image/Image.java
@@ -16,26 +16,77 @@ public class Image {
      */
     public static void main(String[] args) {
         // CONSTANTES
-        final int NL=2,NC=2;
+        final int NL=5,NC=6;
         // VARIABLES
 
         // initialisation de la matrice image
-        int[][] M = new int[][] {
-                                    {5,0},
-                                    {0,100}
-                                    };       
-        AfficheTableau(M);            
+        int[][] T = new int[][] {
+                                    {5,50},
+                                    {75,40}
+                                    };   
+        
+                int[][] M = new int[][] {
+                            {25,	28,	100,	100,	100,	100},
+                            {44,	45,	50,	100,	100,	100},
+                            {65,	65,	50,	20,	100,	100 },
+                            {90,	62,	100,	5,	0,	0},
+                            {60,	87,	100,	0,	100,	100}
+                                    }; 
+
+        AfficheTableau(M);
+        System.out.println("Matrice Image");
+        // matrice image inversé pour image négative
+        Mystere(M);
+        AfficheTableau(M);
+                System.out.println("Matrice Négative");
+
+        ContrastPlus(M);
+        AfficheTableau(M);
+                System.out.println("Matrice Image Contrast Plus");
     }
 
-        
+    public static int[][] Mystere(int[][] tab) {
+        // Matrice : tableau d’entiers de 5 lignes et 6 colonnes
+        // CONSTANTES
+        final int NL=5,NC=6;
+        for (int i = 0; i < NL; i++) {
+            for (int j = 0; j < NC; j++) {
+                tab[i][j] = 100 - tab[i][j];
+            }
+        }
+        return tab;
+    }
 
+    public static int[][] ContrastPlus(int[][] tab) {
+        // Matrice : tableau d’entiers de 5 lignes et 6 colonnes
+        // CONSTANTES
+        final int NL=5,NC=6;
+        // VARIABLES
+        int cp;
+        
+        for (int i = 0; i < NL; i++) {
+            for (int j = 0; j < NC; j++) {
+                if ( tab[i][j] > 75 ) {
+                    cp = 100;
+                } else if ( tab[i][j] > 50 ) {
+                    cp = 75;
+                } else {
+                    cp = tab[i][j]/2;
+                }
+                tab[i][j] = cp;
+            }
+        }
+        return tab;
+    }
     
     public static void AfficheTableau(int[][] tab) {
         // Affiche Tableau Dimension 2 ordre 5 
-        for (int i = 0; i < 2; i++) {
+        // CONSTANTES
+        final int NL=5,NC=6;
+        for (int i = 0; i < NL; i++) {
             System.out.println();
-            for (int j = 0; j < 2; j++) {
-                System.out.print(tab[i][j] + " ");
+            for (int j = 0; j < NC; j++) {
+                System.out.print(tab[i][j] + " \t");
             }
         }
     }
