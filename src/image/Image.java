8a0e21b940eaf2ffcc9c9eebcae10102af1d66f7
Jc Tarot <jctarot.gep@gmail.com>
2020-01-27 11:33:43 +0100

    app Image version 0.1

--- src/image/Image.java
+++ src/image/Image.java
@@ -7,7 +7,7 @@ package image;
 
 /**
  *
- * @author formation_gep
+ * @author Jean-Christophe Tarot formation_ep
  */
 public class Image {
 
@@ -15,7 +15,28 @@ public class Image {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // CONSTANTES
+        final int NL=2,NC=2;
+        // VARIABLES
+
+        // initialisation de la matrice image
+        int[][] M = new int[][] {
+                                    {5,0},
+                                    {0,100}
+                                    };       
+        AfficheTableau(M);            
     }
+
+        
+
     
+    public static void AfficheTableau(int[][] tab) {
+        // Affiche Tableau Dimension 2 ordre 5 
+        for (int i = 0; i < 2; i++) {
+            System.out.println();
+            for (int j = 0; j < 2; j++) {
+                System.out.print(tab[i][j] + " ");
+            }
+        }
+    }
 }
